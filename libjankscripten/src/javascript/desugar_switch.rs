15f0aa8641ccee6e115e0022f6aed47e54b58c32
Jenna Hammond <jennachamond@gmail.com>
2020-05-29 14:49:24 -0400

    Added block/break labels

--- libjankscripten/src/javascript/desugar_switch.rs
+++ libjankscripten/src/javascript/desugar_switch.rs
@@ -7,15 +7,37 @@ use resast::LogicalOp;
 
 struct SwitchToIf { }
 
-// TODO: figure out break labeling
+
+// NOTE (jenna): i don't like all this cloning but also don't know enough
+// about rust to know how to fix it right now 
+fn name_breaks(stmts: &Stmt, name: &Id) -> Stmt {
+    let mut new_vec = vec![];
+    match stmts {
+        Stmt::Block(v) => {
+            for s in v {
+                match s {
+                    Stmt::Break(None) => {
+                        new_vec.push(Stmt::Break(Some(name.clone())));
+                    } 
+                    _ => {
+                        new_vec.push(s.clone());
+                    }
+                }
+            }
+        }
+        _ => { 
+            // I believe the parser only produces blocks 
+            // when pairing stmts with cases so this doesnt matter
+        }
+    }
+    Stmt::Block(new_vec)
+}
 
 impl Visitor for SwitchToIf {
-    /// called before recursing on a statement
-    fn enter_stmt(&mut self, _stmt: &mut Stmt) {}
-    /// called before recursing on an expression
-    fn enter_expr(&mut self, _expr: &mut Expr, _loc: &mut Loc) {}
-    /// called after recursing on a statement, with the new value
     fn exit_stmt(&mut self, stmt: &mut Stmt) {
+        let mut ng = NameGen::default();
+        let name = ng.fresh("switch");
+
         match stmt {
             Stmt::Switch(expr, cases, default) => { //cases = vec<(expr, stmt)>
                 let test = &**expr;
@@ -35,7 +57,7 @@ impl Visitor for SwitchToIf {
                                     Box::new(Expr::Id(Id::Named("test".to_string()))),
                                     Box::new(e.clone()))),
                                 Box::new(Expr::Id(Id::Named("fallthrough".to_string()))))),
-                            Box::new(s.clone()),
+                            Box::new(name_breaks(s, &name)),
                             Box::new(Stmt::Empty)))
                 }
 
@@ -52,15 +74,20 @@ impl Visitor for SwitchToIf {
 
                 //create labeled block w if statements/default 
                 *stmt = Stmt::Label(
-                    Id::Named("sw".to_string()),
+                    name,
                     Box::new(Stmt::Block(v)))
             }
             _ => {}
         }
     }
-    /// called after recursing on an expression, with the new value
-    fn exit_expr(&mut self, _expr: &mut Expr, _loc: &mut Loc) {}
 }
+
+// TODO (jenna): testing -- current "tests" for own use 
+// Notes:
+// * won't be able to get an Id::Generated just by parsing ifs, so
+//   assert-eq! won't work if doing that to compare 
+// * not sure if all possible breaks will be handled -- need to look
+//   into break behavior some more 
 
 #[test] 
 fn parse_switch() {
