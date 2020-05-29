b68dda31675c39a59de91caa7d7841035f08bea0
Jenna Hammond <jennachamond@gmail.com>
2020-05-29 12:48:38 -0400

    Simplified code

--- libjankscripten/src/javascript/simpl_fancy_updates.rs
+++ libjankscripten/src/javascript/simpl_fancy_updates.rs
@@ -1,5 +1,6 @@
 use super::syntax::*;
 use super::walk::*;
+use super::parser::parse;
 use resast::BinaryOp;
 
 struct MyVisitor { }
@@ -12,129 +13,56 @@ fn lval_to_expr(lv: &mut LValue) -> Expr {
     }
 }
 
+fn desugar_assign_op(bin_op: BinaryOp, lv: &mut LValue, rhs: &mut Expr) -> Expr {
+    Expr::Assign(
+        AssignOp::Equal,
+        Box::new(lv.clone()),
+        Box::new(
+            Expr::Binary(
+                BinOp::BinaryOp(bin_op),
+                Box::new(lval_to_expr(lv)),
+                Box::new(rhs.take()))))
+}
+
 impl Visitor for MyVisitor {
     fn exit_expr(&mut self, expr: &mut Expr, _loc: &Loc) {
         match expr {
             Expr::Assign(AssignOp::Equal, _lv, _rhs) => { }
             Expr::Assign(AssignOp::PlusEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::Plus),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::Plus, lv, rhs);
             },
             Expr::Assign(AssignOp::MinusEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::Minus),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::Minus, lv, rhs);
             },
             Expr::Assign(AssignOp::TimesEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::Times),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::Times, lv, rhs);
             },
             Expr::Assign(AssignOp::DivEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::Over),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::Over, lv, rhs);
             },
             Expr::Assign(AssignOp::ModEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::Mod),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::Mod, lv, rhs);
             },
             Expr::Assign(AssignOp::LeftShiftEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::LeftShift),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::LeftShift, lv, rhs);
             },
             Expr::Assign(AssignOp::RightShiftEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::RightShift),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::RightShift, lv, rhs);
             },
             Expr::Assign(AssignOp::UnsignedRightShiftEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::UnsignedRightShift),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::UnsignedRightShift, lv, rhs);
             },
             Expr::Assign(AssignOp::OrEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::Or),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::Or, lv, rhs);
             },
             Expr::Assign(AssignOp::XOrEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::XOr),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::XOr, lv, rhs);
             },
             Expr::Assign(AssignOp::AndEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::And),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::And, lv, rhs);
             },
             Expr::Assign(AssignOp::PowerOfEqual, lv, rhs) => {
-                *expr = Expr::Assign(
-                    AssignOp::Equal,
-                    lv.clone(),
-                    Box::new(
-                        Expr::Binary(
-                            BinOp::BinaryOp(BinaryOp::PowerOf),
-                            Box::new(lval_to_expr(lv.as_mut())),
-                            Box::new(rhs.take()))));
+                *expr = desugar_assign_op(BinaryOp::PowerOf, lv, rhs);
             }
             _ => {}
         }
@@ -149,215 +77,168 @@ pub fn simpl(program: &mut Stmt) {
 
 #[test]
 fn parse_pluseq() {    
-    let mut expr = Expr::Assign(
-        AssignOp::PlusEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::Plus), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x += 1;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x + 1;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_minuseq() {
-    let mut expr = Expr::Assign(
-        AssignOp::MinusEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::Minus), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x -= 1;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x - 1;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_timeseq() {
-    let mut expr = Expr::Assign(
-        AssignOp::TimesEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(BinOp::BinaryOp(BinaryOp::Times), 
-        Box::new(Expr::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x *= 1;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x * 1;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_diveq() {
-    let mut expr = Expr::Assign(
-        AssignOp::DivEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::Over), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x /= 1;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x / 1;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_modeq() {
-    let mut expr = Expr::Assign(
-        AssignOp::ModEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::Mod), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x %= 1;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x % 1;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_lshifteq() {
-    let mut expr = Expr::Assign(
-        AssignOp::LeftShiftEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::LeftShift), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x <<= 1;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x << 1;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_rshifteq() {
-    let mut expr = Expr::Assign(
-        AssignOp::RightShiftEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::RightShift), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x >>= 1;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x >> 1;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_unsignedrshifteq() {
-    let mut expr = Expr::Assign(
-        AssignOp::UnsignedRightShiftEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::UnsignedRightShift), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x >>>= 1;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x >>> 1;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_expeq() {
-    let mut expr = Expr::Assign(
-        AssignOp::PowerOfEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::PowerOf), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x **= 1;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x ** 1;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_oreq() {
-    let mut expr = Expr::Assign(
-        AssignOp::OrEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::Or), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x |= true;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x | true;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_xoreq() {
-    let mut expr = Expr::Assign(
-        AssignOp::XOrEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::XOr), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
+    let mut prog = parse(r#"
+        x ^= true;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x ^ true;
+    "#).unwrap();
     
-    assert_eq!(expr, result);
+    assert_eq!(prog, result);
 }
 
 #[test]
 fn parse_andeq() {
-    let mut expr = Expr::Assign(
-        AssignOp::AndEqual, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Lit(Lit::Num(Num::Int(1)))));
-    let result = Expr::Assign(
-        AssignOp::Equal, 
-        Box::new(LValue::Id(Id::Named("x".to_string()))), 
-        Box::new(Expr::Binary(
-            BinOp::BinaryOp(BinaryOp::And), 
-            Box::new(Expr::Id(Id::Named("x".to_string()))), 
-            Box::new(Expr::Lit(Lit::Num(Num::Int(1)))))));
-    expr.walk(&mut MyVisitor {});
-    
-    assert_eq!(expr, result);
+    let mut prog = parse(r#"
+        x &= true;
+    "#).unwrap();
+    simpl(&mut prog);
+
+    let result = parse(r#"
+        x = x & true;
+    "#).unwrap();
+
+    assert_eq!(prog, result);
 }
