47983260021ba152262eea302a0549d8efe0a0b3
Jenna Hammond <jennachamond@gmail.com>
2020-06-02 14:39:44 -0400

    Initial commit

---
+++ libjankscripten/src/javascript/desugar_functions.rs
@@ -0,0 +1,4 @@
+// We name all functions using “var”. Note that this involves “lifting” function declarations.
+// We name the result of all function applications.
+
+//struct NameFunctions {ng: NameGen}
