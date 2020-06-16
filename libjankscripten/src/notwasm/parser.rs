52b95d397daf7e3f0d58f25b533106bca9cbe846
Arjun Guha <arjun@cs.umass.edu>
2020-06-16 14:18:11 -0400

    Use combine-language for the parser
    - Better error locations (not necessarily due to combine-language)
    - More robust handing of keywords
    - Less code

--- libjankscripten/src/notwasm/parser.rs
+++ libjankscripten/src/notwasm/parser.rs
@@ -2,206 +2,69 @@
 use super::syntax::*;
 
 use std::collections::HashMap;
+use combine_language::{LanguageDef, LanguageEnv, Identifier};
 use combine::error::ParseError;
 use combine::parser;
-use combine::parser::char::{alpha_num, char, digit, spaces, string};
+use combine::parser::char::{alpha_num, char, digit, letter, spaces, string};
 use combine::stream::easy;
 use combine::stream::Stream;
+use combine::stream::state::State;
+
 use combine::{
     attempt, between, eof, many, many1, optional, satisfy_map, sep_end_by, token, Parser,
-    sep_by, value, chainl1
+    sep_by, value, chainl1, satisfy
 };
 use std::fmt;
 
-#[derive(Debug, PartialEq, Clone)]
-enum Tok {
-    LBrace,
-    RBrace,
-    LParen,
-    RParen,
-    Op(&'static str),
-    Keyword(&'static str),
-    Plus,
-    Minus,
-    Equalequal,
-    If,
-    Return,
-    Else,
-    Var,
-    Semi,
-    Comma,
-    Colon,
-    Equals,
-    Function,
-    I32,
-    Int32(i32),
-    Bool(bool),
-    Id(String),
-    Eof,
-}
-
-impl fmt::Display for Tok {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use Tok::*;
-        match self {
-            LBrace => write!(f, "{{"),
-            RBrace => write!(f, "}}"),
-            LParen => write!(f, "("),
-            RParen => write!(f, ")"),
-            Plus => write!(f, "+"),
-            Minus => write!(f, "-"),
-            Equalequal => write!(f, "=="),
-            If => write!(f, "if"),
-            Return => write!(f, "return"),
-            Else => write!(f, "else"),
-            Function => write!(f, "function"),
-            Var => write!(f, "var"),
-            Semi => write!(f, ";"),
-            Comma => write!(f, ","),
-            Colon => write!(f, ":"),
-            Equals => write!(f, "="),
-            I32 => write!(f, "i32"),
-            Int32(i) => write!(f, "{}", i),
-            Id(x) => write!(f, "{}", &x),
-            Bool(b) => write!(f, "{}", b),
-            Op(s) => write!(f, "{}", s),
-            Keyword(s) => write!(f, "{}", s),
-            Eof => write!(f, "end-of-input"),
-        }
-    }
-}
-
-fn lex(s: &str) -> Result<Vec<Tok>, easy::ParseError<&str>> {
-    // The string(..) parser consumes its input eagerly. For example,
-    // string("AA") will fail on the input "AB", with "A" consumed. Therefore,
-    // string("AA").or(string("AB")) cannot parse the input "AB", since the
-    // left-hand side of the .or(..) consumes the "A". The simplest fix is
-    // to write attempt(string("AA")).or(string("AB")). The attempt combinator
-    // backtracks the input state if its argument fails to parse the input.
-
-    let tok = // Symbols
-        string("{").map(|_x| Tok::LBrace)
-        .or(string(">").with(value(Tok::Op(">"))))
-        .or(string("}").map(|_x| Tok::RBrace))
-        .or(string("(").map(|_x| Tok::LParen))
-        .or(string(")").map(|_x| Tok::RParen))
-        .or(string(",").map(|_x| Tok::Comma))
-        .or(string(";").map(|_x| Tok::Semi))
-        .or(string(":").map(|_x| Tok::Colon))
-        .or(string("+").map(|_x| Tok::Plus))
-        .or(string("-").map(|_x| Tok::Minus))
-        .or(attempt(string("==")).map(|_x| Tok::Equalequal))
-        .or(string("=").map(|_x| Tok::Equals))
-        // Keywords in alphabetical order
-        .or(attempt(string("else")).map(|_x| Tok::Else))
-        .or(attempt(string("false")).map(|_x| Tok::Bool(false)))
-        .or(attempt(string("function")).map(|_x| Tok::Function))
-        .or(attempt(string("i32")).map(|_x| Tok::I32))
-        .or(attempt(string("if")).map(|_x| Tok::If))
-        .or(attempt(string("loop")).with(value(Tok::Keyword("loop"))))
-        .or(attempt(string("break")).with(value(Tok::Keyword("break"))))
-        .or(attempt(string("return")).map(|_x| Tok::Return))
-        .or(attempt(string("true")).map(|_x| Tok::Bool(true)))
-        .or(attempt(string("var")).map(|_x| Tok::Var))
-        // Numbers
-        .or((optional(char('-')), many1(digit())).map(
-            |(sign, digits): (Option<char>, String)| {
-                let n = digits.parse::<i32>().unwrap();
-                match sign {
-                    Some('-') => Tok::Int32(-n),
-                    _ => Tok::Int32(n),
-                }
-            },
-        ))
-        // Identifiers
-        .or(many1(alpha_num()).map(|x: String| Tok::Id(x)));
-
-    let ws = spaces();
-
-    let mut toks = spaces()
-        .with(sep_end_by(tok, ws))
-        .skip(eof())
-        .map(|mut tokens: Vec<Tok>| {
-            tokens.push(Tok::Eof);
-            tokens
-        });
-    toks.easy_parse(s).map(|tuple| tuple.0)
-}
-
-
-fn op<I>(op: &'static str) -> impl Parser<Input = I, Output = ()>
-where
-    I: Stream<Item = Tok>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    satisfy_map(move |t| match t {
-        Tok::Op(op_) if op == op_ => Some(()),
-        _ => None
-    })
-}
-
-fn kw<I>(kw: &'static str) -> impl Parser<Input = I, Output = ()>
-where
-    I: Stream<Item = Tok>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    satisfy_map(move |t| match t {
-        Tok::Keyword(kw_) if kw == kw_ => Some(()),
-        _ => None
-    })
-}
-
-fn lit<I>() -> impl Parser<Input = I, Output = Lit>
-where
-    I: Stream<Item = Tok>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    satisfy_map(|t| match t {
-        Tok::Int32(n) => Some(Lit::I32(n)),
-        Tok::Bool(b) => Some(Lit::Bool(b)),
-        _ => None,
-    })
-}
-
-fn id<I>() -> impl Parser<Input = I, Output = Id>
-where
-    I: Stream<Item = Tok>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    satisfy_map(|t| match t {
-        Tok::Id(x) => Some(Id::Named(x)),
-        _ => None,
-    })
-}
+type Lang<'a, I> = LanguageEnv<'a, I>;
 
 parser! {
-    fn binop[I]()(I) -> BinaryOp
-    where [I: Stream<Item = Tok>]
+    fn lit['a, 'b, I](lang: &'b Lang<'a, I>)(I) -> Lit
+    where [I: Stream<Item = char>]
     {
-        token(Tok::Plus).with(value(BinaryOp::I32Add))
-        .or(op(">").with(value(BinaryOp::I32GT)))
-        .or(token(Tok::Minus).with(value(BinaryOp::I32Eq)))
-        .or(token(Tok::Equalequal).with(value(BinaryOp::I32Sub)))
+        lang.integer().map(|n| Lit::I32(n as i32))
+        .or(lang.reserved("true").with(value(Lit::Bool(true))))
+        .or(lang.reserved("false").with(value(Lit::Bool(false))))
+
     }
 }
 
 parser! {
-    fn atom[I]()(I) -> Atom
-    where [I: Stream<Item = Tok>]
+    fn id['a, 'b, I](lang: &'b Lang<'a, I>)(I) -> Id
+    where [I: Stream<Item = char>]
+    {
+        lang.identifier().map(|x| Id::Named(x.to_string()))
+    }
+}
+
+parser! {
+    fn binop['a, 'b, I](lang: &'b Lang<'a, I>)(I) -> BinaryOp
+    where [I: Stream<Item = char>]
+    {
+        lang.reserved_op("+").with(value(BinaryOp::I32Add))
+        .or(lang.reserved_op(">").with(value(BinaryOp::I32GT)))
+        .or(lang.reserved_op("-").with(value(BinaryOp::I32Eq)))
+        .or(lang.reserved_op("==").with(value(BinaryOp::I32Sub)))
+    }
+}
+
+parser! {
+    fn atom['a, 'b, I](lang: &'b Lang<'a, I>)(I) -> Atom
+    where [I: Stream<Item = char>]
     {
         chainl1(
-            atom_item(),
-            binop().map(|op| move |lhs, rhs| Atom::Binary(op, Box::new(lhs), Box::new(rhs), Type::I32)))
+            atom_item(lang),
+            binop(lang).map(|op| move |lhs, rhs| Atom::Binary(op, Box::new(lhs), Box::new(rhs), Type::I32)))
     }    
 }
 
 parser! {
-    fn atom_item[I]()(I) -> Atom
-    where [I: Stream<Item = Tok>]
+    fn atom_item['a, 'b, I](lang: &'b Lang<'a, I>)(I) -> Atom
+    where [I: Stream<Item = char>]
     {
-        lit().map(|l| Atom::Lit(l))
-        .or(id().map(|x| Atom::Id(x)))
-        .or(between(token(Tok::LParen), token(Tok::RParen), atom()))
+        lit(lang).map(|l| Atom::Lit(l))
+        .or(id(lang).map(|x| Atom::Id(x)))
+        .or(lang.parens(atom(lang)))
     }
 }
 
@@ -210,124 +73,148 @@ enum AfterId {
     Op(BinaryOp, Atom)
 }
 
-fn expr<I>() -> impl Parser<Input = I, Output = Expr>
-where
-    I: Stream<Item = Tok>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    id()
-        .and(optional(
-            between(token(Tok::LParen), token(Tok::RParen), sep_by(id(), token(Tok::Comma))).map(|args| AfterId::Args(args))
-            .or(binop().and(atom()).map(|(op, e)| AfterId::Op(op, e)))))
+parser! {
+    fn expr['a, 'b, I](lang: &'b Lang<'a, I>)(I) ->  Expr
+    where [ I: Stream<Item = char>]
+    {
+        id(lang)
+        .and(optional(lang.parens(sep_by(id(lang), lang.reserved_op(","))).map(|args| AfterId::Args(args))
+            .or(binop(lang).and(atom(lang)).map(|(op, e)| AfterId::Op(op, e)))))
         .map(|(f, opt_args)| match opt_args {
             None => Expr::Atom(Atom::Id(f)),
             Some(AfterId::Args(args)) => Expr::CallDirect(f, args),
             Some(AfterId::Op(op, e)) => Expr::Atom(Atom::Binary(op, Box::new(Atom::Id(f)), Box::new(e), Type::I32))
         })
-        .or(atom().map(|a| Expr::Atom(a)))
-}
-
-fn type_<I>() -> impl Parser<Input = I, Output = Type>
-where
-    I: Stream<Item = Tok>,
-    I::Error: ParseError<I::Item, I::Range, I::Position>,
-{
-    satisfy_map(|t| match t {
-        Tok::I32 => Some(Type::I32),
-        _ => None,
-    })
-    // unimplemented!();
-}
-
-parser! {
-    fn stmt[I]()(I) -> Stmt
-    where [I: Stream<Item = Tok>]
-    {
-        let var = token(Tok::Var)
-            .with(id())
-            .skip(token(Tok::Colon))
-            .and(type_())
-            .skip(token(Tok::Equals))
-            .and(expr())
-            .skip(token(Tok::Semi))
-            .map(|((x,t),e)| Stmt::Var(x, e, t));
-
-        let assign = id()
-            .skip(token(Tok::Equals))
-            .and(expr())
-            .skip(token(Tok::Semi))
-            .map(|(x,e)| Stmt::Assign(x, e));
-
-        let if_ = token(Tok::If)
-            .with(between(token(Tok::LParen), token(Tok::RParen), atom()))
-            .and(block())
-            .skip(token(Tok::Else))
-            .and(block())
-            .map(|((a, s1), s2)| Stmt::If(a, Box::new(s1), Box::new(s2)));
-        
-        let loop_ = kw("loop")
-            .with(block())
-            .map(|s| Stmt::Loop(Box::new(s)));
-
-        let return_ = token(Tok::Return)
-            .with(atom())
-            .skip(token(Tok::Semi))
-            .map(|a| Stmt::Return(a));
-
-        let break_ = kw("break")
-            .with(id())
-            .skip(token(Tok::Semi))
-            .map(|l| Stmt::Break(l));
-
-        var.or(assign).or(if_).or(return_).or(block()).or(loop_).or(break_)
+        .or(atom(lang).map(|a| Expr::Atom(a)))
     }
 }
 
 parser! {
-    fn block[I]()(I) -> Stmt
-    where [I: Stream<Item = Tok>]
+    fn type_['a, 'b, I](lang: &'b Lang<'a, I>)(I) -> Type
+    where [ I: Stream<Item = char>]
     {
-        between(token(Tok::LBrace), token(Tok::RBrace), many(stmt()))
+        lang.reserved("i32").with(value(Type::I32))
+    }
+}
+
+parser! {
+    fn stmt['a, 'b, I](lang: &'b Lang<'a,I>)(I) -> Stmt
+    where [I: Stream<Item = char>]
+    {
+        let var = lang.reserved("var")
+            .with(id(lang))
+            .skip(lang.reserved_op(":"))
+            .and(type_(lang))
+            .skip(lang.reserved_op("="))
+            .and(expr(lang))
+            .skip(lang.reserved_op(";"))
+            .map(|((x,t),e)| {
+                let x = Stmt::Var(x, e, t);
+                println!("{:?}", x);
+                x
+            });
+
+        let assign = id(lang)
+            .skip(lang.reserved_op("="))
+            .and(expr(lang))
+            .skip(lang.reserved_op(";"))
+            .map(|(x,e)| Stmt::Assign(x, e));
+
+        let if_ = lang.reserved("if")
+            .with(lang.parens(atom(lang)))
+            .and(block(lang))
+            .skip(lang.reserved("else"))
+            .and(block(lang))
+            .map(|((a, s1), s2)| Stmt::If(a, Box::new(s1), Box::new(s2)));
+        
+        let loop_ = lang.reserved("loop")
+            .with(block(lang))
+            .map(|s| Stmt::Loop(Box::new(s)));
+
+        let return_ = lang.reserved("return")
+            .with(atom(lang))
+            .skip(lang.reserved_op(";"))
+            .map(|a| Stmt::Return(a));
+
+        let break_ = lang.reserved("break")
+            .with(id(lang))
+            .skip(lang.reserved_op(";"))
+            .map(|l| Stmt::Break(l));
+
+        var.or(if_).or(return_).or(block(lang)).or(loop_).or(break_).or(assign)
+    }
+}
+
+parser! {
+    fn block['a, 'b, I](lang: &'b Lang<'a,I>)(I) -> Stmt
+    where [I: Stream<Item = char>]
+    {
+        lang.braces(many(stmt(lang)))
             .map(|ss| Stmt::Block(ss))
     }
 }
 
 parser! {
-    fn function[I]()(I) -> (Id, Function)
-    where [I: Stream<Item = Tok>]
+    fn function['a, 'b, I](lang: &'b Lang<'a,I>)(I) -> (Id, Function)
+    where [I: Stream<Item = char>]
     {
-        token(Tok::Function)
-            .with(id())
-            .and(
-                between(
-                    token(Tok::LParen),
-                    token(Tok::RParen),
-                    sep_by(id().skip(token(Tok::Colon)).and(type_()), token(Tok::Comma))))
-            .skip(token(Tok::Colon))
-            .and(type_())
-            .and(block())
-            .map(|(((f, params_tys), ret_ty), body)| 
-            (f, Function { locals: vec![], body, params_tys, ret_ty }))
+        lang.reserved("function")
+        .with(id(lang))
+        .and(lang.parens(
+            sep_by(id(lang).skip(lang.reserved_op(":")).and(type_(lang)), lang.reserved_op(","))))
+        .skip(lang.reserved_op(":"))
+        .and(type_(lang))
+        .and(block(lang))
+        .map(|(((f, params_tys), ret_ty), body)| 
+        (f, Function { locals: vec![], body, params_tys, ret_ty }))
     }
 }
 
 parser! {
-    fn program[I]()(I) -> Program
-    where [I: Stream<Item = Tok>]
+    fn program['a, 'b, I](lang: &'b Lang<'a,I>)(I) -> Program
+    where [I: Stream<Item = char>]
     {
-        many::<HashMap<_, _>, _>(function())
-            .skip(token(Tok::Eof))
-            .map(|functions| {
-                let classes = HashMap::new();
-                let globals = HashMap::new();
-                Program { functions, classes, globals }
-            })
+        lang.white_space()
+        .with(many::<HashMap<_, _>, _>(function(lang)))
+        .skip(eof())
+        .map(|functions| {
+            let classes = HashMap::new();
+            let globals = HashMap::new();
+            Program { functions, classes, globals }
+        })
     }
 }
 
 pub fn parse(input: &str) -> Program {
-    let tokens = lex(input).expect("lexing error");
-    let program = program().easy_parse(&tokens[..]).expect("parsing error");
-    program.0
+    // NOTE(arjun): It would be nice to extract this language definition into
+    // a function. But, I have no idea what its type should be, since the
+    // input type is not &str, but a "stream" (defined in the combine library).
+    let lang = LanguageEnv::new(LanguageDef {
+        ident: Identifier {
+            start: letter(),
+            rest: alpha_num(),
+            reserved: ["if", "else", "true", "false", "function", "loop", 
+                "return", "i32" ]
+                .iter()
+                .map(|x| (*x).into())
+                .collect(),
+        },
+        // NOTE(arjun): This is from the combine-language documentation. I have
+        // not bothered to understand it. But, it ought to define a pattern that
+        // matches operators. Our operators are quite straightforward.
+        op: Identifier {
+            start: satisfy(|c| "+-*/".chars().any(|x| x == c)),
+            rest: satisfy(|c| "+-*/".chars().any(|x| x == c)),
+            reserved: ["+", "-", "*", "/"].iter().map(|x| (*x).into()).collect()
+        },
+        comment_start: string("/*").map(|_| ()),
+        comment_end: string("*/").map(|_| ()),
+        comment_line: string("//").map(|_| ()),
+    });
+
+    let mut parser = program(&lang);
+    let input_stream = State::new(input);
+    let p = parser.easy_parse(input_stream).expect("parsing error");
+    return p.0;
 }
 
