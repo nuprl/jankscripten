cf2408dd0e7a920355d0f4f3b4f745b348170b7b
Mark Aldrich <mastermark51200@gmail.com>
2020-07-22 16:02:50 -0400

    JankyScript: more typing rules & mechanisms

--- libjankscripten/src/jankyscript/type_checking.rs
+++ libjankscripten/src/jankyscript/type_checking.rs
@@ -11,6 +11,7 @@ type Env = HashMap<Id, Type>;
 #[derive(Debug)]
 pub enum TypeCheckingError {
     TypeMismatch(String, Type, Type),
+    ExpectedFunction(String, Type),
 }
    
 pub type TypeCheckingResult<T> = Result<T, TypeCheckingError>;
@@ -28,8 +29,18 @@ fn ensure(msg: &str, expected: Type, got: Type) -> TypeCheckingResult<Type> {
     }
 }
 
+fn ensure_function(msg: &str, got: Type) -> TypeCheckingResult<(Vec<Type>, Box<Type>)> {
+    match got {
+        Type::Function(args_types, return_type) => {
+            Ok((args_types, return_type))
+        },
+        _ => Err(TypeCheckingError::ExpectedFunction(String::from(msg), got))
+    }
+}
+
 // type check an entire program.
 pub fn type_check(stmt: Stmt) -> TypeCheckingResult<()> {
+    
     match type_check_stmt(stmt, HashMap::new()) {
         Ok(_) => Ok(()),
         Err(error) => Err(error)
@@ -55,7 +66,13 @@ fn type_check_stmt(stmt: Stmt, env: Env) -> TypeCheckingResult<Env> {
 
             Ok(env)
         },
-        Stmt::Block(stmts) => type_check_stmts(stmts, env),
+        Stmt::Block(stmts) => {
+            type_check_stmts(stmts, env.clone())?;
+            Ok(env)
+        },
+        Stmt::Return(_) => {
+            todo!("type_check_stmt should take optional ret_ty");
+        }
         _ => unimplemented!()
     }
 }
@@ -76,16 +93,47 @@ fn type_check_expr(expr: Expr, env: Env) -> TypeCheckingResult<Type> {
             let mut function_env = env.clone();
             function_env.extend(args.clone().into_iter());
 
+            match *body {
+                Stmt::Block(stmts) => {
+                    todo!("gather all variables defined in this function without crossing a function definition boundary, lift their definitions to the top");
+                },
+                _ => panic!()
+            }
+
             type_check_stmt(*body, function_env)?;
 
             Ok(Type::Function(
-                args.into_iter().map(|(name, ty)| ty).collect(), 
+                args.into_iter().map(|(_, ty)| ty).collect(), 
                 Box::new(return_type)))
         },
         Expr::Call(fun, args) => {
-            // let fun_type = 
-            unimplemented!();
+            // ensure that `fun` is a function.
+            // get its expected argument types.
+            let (expected_arg_types, return_type) = 
+                ensure_function("expected function for function call",
+                    type_check_expr(*fun, env.clone())?)?;
+
+            // derive types for the actual arguments.
+            let actual_arg_types: Vec<TypeCheckingResult<Type>> = 
+                args.into_iter().map(|e| type_check_expr(e, env.clone())).collect();
+
+            // now we want to iterate over the expected arg types and 
+            // actual arg types. we'll create a zipped iterator to
+            // simultaneously iterate over both.
+            let actual_and_expected_arg_types = actual_arg_types.into_iter()
+                .zip(expected_arg_types.into_iter());
+
+            for (actual, expected) in actual_and_expected_arg_types {
+                ensure("function argument must match declared type", 
+                    expected,
+                    actual?)?;
+            }
+
+            Ok(*return_type)
         },
+        Expr::Coercion(coercion, e) => {
+            unimplemented!();
+        }
         _ => unimplemented!()
     }
 }
