148bebbab160db738440d1ef005bae76eba88335
Arjun Guha <arjun@cs.umass.edu>
2020-08-05 11:48:41 -0400

    Document issues with assignment desugaring

--- libjankscripten/src/javascript/desugar_updates.rs
+++ libjankscripten/src/javascript/desugar_updates.rs
@@ -49,6 +49,23 @@ impl Visitor for DesugarFancyUpdates {
             Expr::Assign(AssignOp::PowerOfEqual, lv, rhs) => {
                 *expr = desugar_assign_op(BinaryOp::PowerOf, lv, rhs);
             }
+            Expr::UnaryAssign(_op, _e) => {
+                // There are four essential cases
+                //
+                // ++x => x = x + 1, x
+                //
+                // ++e.x => tmp = e, tmp.x = tmp.x + 1, tmp.x      introduce tmp
+                //
+                // x++ => tmp = x, x = x + 1, tmp                  introduce tmp
+                //
+                // e.x++ => tmp1 = e, tmp2 = tmp1.x,              introduce tmp1
+                //           tmp1.x = tmp1.x + 1, tmp2                  and tmp2
+                //
+                // The decrement operators are similar to the increment
+                // operators. Computed field lookups (e1[e2]) may require a new
+                // temporary variable too.
+                todo!("unary assignment operators")
+            }
             _ => {
                 //not an assignment, proceed as usual
             }
@@ -59,12 +76,20 @@ impl Visitor for DesugarFancyUpdates {
 fn lval_to_expr(lv: &mut LValue) -> Expr {
     match lv {
         LValue::Id(x) => id_(x.clone()),
-        LValue::Dot(e, x) => dot_(e.clone(), x.clone()),
-        LValue::Bracket(e1, e2) => bracket_(e1.clone(), e2.clone()),
+        LValue::Dot(e, x) => {
+            assert!(e.is_essentially_atom(), "potentially duplicating effects");
+            dot_(e.clone(), x.clone())
+        }
+        LValue::Bracket(e1, e2) => {
+            assert!(e1.is_essentially_atom(), "potentially duplicating effects");
+            assert!(e2.is_essentially_atom(), "potentially duplicating effects");
+            bracket_(e1.clone(), e2.clone())
+        }
     }
 }
 
 fn desugar_assign_op(bin_op: BinaryOp, lv: &mut LValue, rhs: &mut Expr) -> Expr {
+    // TODO(arjun): This is broken. lv cloning is definitely an issue
     assign_(
         lv.clone(),
         binary_(BinOp::BinaryOp(bin_op), lval_to_expr(lv), rhs.take()),
