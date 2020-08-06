4df5972f32943eba4abd254baa08b9b2caadb0b0
Arjun Guha <arjun@cs.umass.edu>
2020-08-06 18:06:43 +0000

    Function to convert Janky types to NotWasm types

--- libjankscripten/src/shared/types.rs
+++ libjankscripten/src/shared/types.rs
@@ -1,5 +1,7 @@
 //! The types associated with the JankyScript language.
 
+use crate::notwasm::syntax::{FnType, Type as NotWasmType};
+
 // TODO(arjun): should be exactly the same as NotWasm types for a first pass.
 #[derive(Debug, Clone, PartialEq)]
 pub enum Type {
@@ -15,6 +17,22 @@ pub enum Type {
 }
 
 impl Type {
+    pub fn notwasm_typ(&self) -> NotWasmType {
+        match self {
+            Type::Any => NotWasmType::Any,
+            Type::Float => NotWasmType::F64,
+            Type::Int => NotWasmType::I32,
+            Type::Bool => NotWasmType::Bool,
+            Type::Function(arg_typs, ret_ty) => NotWasmType::Fn(FnType {
+                args: arg_typs.iter().map(|t| t.notwasm_typ()).collect(),
+                result: Some(Box::new(ret_ty.notwasm_typ())),
+            }),
+            Type::String => NotWasmType::String,
+            Type::Array => NotWasmType::Array,
+            Type::DynObject => NotWasmType::DynObject,
+        }
+    }
+
     pub fn is_ground(&self) -> bool {
         match self {
             Type::Function(args, result_type) => {
