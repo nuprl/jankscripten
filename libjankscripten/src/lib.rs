10c382f6709bc3be3de543fdb7fccb3c66945414
Mark Aldrich <mark@mwaldrich.io>
2020-08-17 13:44:48 -0400

    JankyScript typechecker now always runs
    closes #115

--- libjankscripten/src/lib.rs
+++ libjankscripten/src/lib.rs
@@ -13,7 +13,6 @@ extern crate combine;
 
 pub fn javascript_to_wasm<F, G>(
     js_code: &str,
-    typecheck: bool,
     inspect_janky: F,
     inspect_notwasm: G,
 ) -> Result<Vec<u8>, Box<dyn std::error::Error>>
@@ -27,9 +26,7 @@ where
     let jankier_ast = jankierscript::from_javascript(js_ast);
     let janky_ast = jankierscript::insert_coercions(jankier_ast)?;
     inspect_janky(&janky_ast);
-    if typecheck {
-        jankyscript::type_checking::type_check(&janky_ast)?;
-    }
+    jankyscript::type_checking::type_check(&janky_ast)?;
     let notwasm_ast = notwasm::from_jankyscript(janky_ast);
     inspect_notwasm(&notwasm_ast);
     let wasm_bin = notwasm::compile(notwasm_ast)?;
