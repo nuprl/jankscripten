73200c16bb9f3d914ee75ab2b889e89f979f8a18
Mark Aldrich <mark@mwaldrich.io>
2020-10-01 17:02:16 -0400

    refactor Object.create to RTSFunction

--- libjankscripten/src/rts_function.rs
+++ libjankscripten/src/rts_function.rs
@@ -28,6 +28,7 @@ pub enum RTSFunction {
     Equal,
     StrictNotEqual,
     NotEqual,
+    ObjectCreate,
 }
 
 impl RTSFunction {
@@ -51,6 +52,7 @@ impl RTSFunction {
             Equal => "janky_equal",
             StrictNotEqual => "janky_strict_not_equal",
             NotEqual => "janky_not_equal",
+            ObjectCreate => "object_create",
         }
     }
 
@@ -90,7 +92,8 @@ impl RTSFunction {
             ModF64 => Function(vec![Float, Float], Box::new(Float)),
             StrictEqual | Equal | StrictNotEqual | NotEqual => {
                 Function(vec![Any, Any], Box::new(Bool))
-            }
+            },
+            ObjectCreate => Function(vec![Any], Box::new(Any)),
         }
     }
 }
@@ -117,6 +120,7 @@ impl std::fmt::Display for RTSFunction {
                 Equal => "==",
                 StrictNotEqual => "!==",
                 NotEqual => "!=",
+                ObjectCreate => "object_create",
             }
         )
     }
