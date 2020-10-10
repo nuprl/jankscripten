a2ddcd29b01cc32b3c3cf009eb9959d3c1bbaa26
Mark Aldrich <mark@mwaldrich.io>
2020-10-10 12:43:06 -0400

    cleaned up jankyp prototype analysis

--- jankyp/src/visitors/PrototypeVisitor.ts
+++ jankyp/src/visitors/PrototypeVisitor.ts
@@ -8,7 +8,8 @@ import * as parser from '@babel/parser';
  * Jankscripten's eventual goal is to implement prototypes using a method
  * similar to vtables. However, we want to avoid *deoptimizing* these vtables
  * at runtime, which we would have to do in order to support full prototypes
- * like V8.
+ * like V8. We also detect bad behavior around function objects, since we 
+ * implement their prototypes in a janky way.
  * 
  * Since we want to avoid deoptimization, this visitor detects 2 forms of bad
  * behavior that would cause deoptimization:
@@ -26,6 +27,8 @@ import * as parser from '@babel/parser';
  *    to their properties. Here's an example of this type of bad behavior:
  * 
  *        let r = new Rectangle(2, 2); Rectangle.prototype.extraMethod = ...;
+ * 
+ * 3. *Dynamic object classing.*
  * 
  * 
  * Here are the different syntax forms we instrument to observe the relevant
@@ -87,6 +90,7 @@ export const PrototypeVisitor: JankyPVisitor = {
                 (${qJankyp.name}).trackPrototype(Function.prototype);
                 (${qJankyp.name}).trackPrototype(Boolean.prototype);
                 (${qJankyp.name}).trackPrototype(Symbol.prototype);
+                (${qJankyp.name}).trackPrototype(String.prototype);
                 `;
 
                 let monkeyPatchStmts = parser.parse(monkeyPatch).program.body;
@@ -95,8 +99,6 @@ export const PrototypeVisitor: JankyPVisitor = {
             }
         },
         AssignmentExpression: {
-            // obj.__proto__ = ???;
-            // obj[field] = ???; => obj[check(field)] = ???;
             exit(path) {
                 // to make typescript happy. i don't think it's possible for
                 // loc to be null.
@@ -104,10 +106,7 @@ export const PrototypeVisitor: JankyPVisitor = {
                     return;
                 }
 
-                // o.p = v;
-                // 1. o[checkField(p)] = v;
-                // 2. (checkForProtoModification(o))[checkField(p)] = v;
-                // 3. (fieldWrite(o, p, v))
+                // o.p = v; ~~> (checkPropWrite(o, p, v))
                 
                 if (t.isMemberExpression(path.node.left)) {
                     let [object, property] = decomposeMemberExpression(path.node.left);
