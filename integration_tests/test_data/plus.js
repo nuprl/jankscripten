cb62cd832b3b506965a94258ef8ee7db9f2f7afc
Mark Aldrich <mark@mwaldrich.io>
2020-11-10 17:52:25 -0500

    plus tests

---
+++ integration_tests/test_data/plus.js
@@ -0,0 +1,18 @@
+// Testing various data type coercions with the + operator
+
+// monotype
+log_any(1 + 1); // 2
+log_any("aaa" + "bbb"); // aaabbb
+log_any(1.2 + 1.2) // 2.4
+
+// polytype with string coercions
+log_any("number " + 1); // number 1
+log_any(1 + " number"); // 1 number
+log_any("float " + 1.2); // float 1.2
+log_any(1.2 + " float"); // 1.2 float
+log_any("boolean " + true); // boolean true
+log_any(true + "boolean"); // true boolean
+log_any("value " + undefined); // value undefined
+log_any(undefined + " value"); // undefined value
+log_any("value " + null); // value null
+log_any(null + " value"); // null value
