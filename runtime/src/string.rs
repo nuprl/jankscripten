685b8aebf7d7f8e43210d3c3d9251befa5c202e4
Mark Aldrich <mark@mwaldrich.io>
2020-11-17 16:17:35 -0500

    factored string append code out

--- runtime/src/string.rs
+++ runtime/src/string.rs
@@ -1,10 +1,20 @@
 //! create managed strings and manipulate them
 
+use crate::heap;
 pub use super::heap_types::StringPtr;
 
 #[no_mangle]
 pub extern "C" fn string_len(string: StringPtr) -> i32 {
     string.len() as i32
+}
+
+#[no_mangle]
+pub extern "C" fn string_append(a: StringPtr, b: StringPtr) -> StringPtr {
+    // combine them
+    let combined: String = format!("{}{}", &*a, &*b);
+
+    // allocate this into a string
+    heap().alloc_str_or_gc(combined.as_str())
 }
 
 #[cfg(test)]
@@ -30,5 +40,18 @@ mod test {
     fn alloc_and_read() {
         init();
         assert_eq!(&*StringPtr::from("lapis"), "lapis");
+    }
+    #[test]
+    #[wasm_bindgen_test]
+    fn string_append_hello_world() {
+        init();
+        let a = StringPtr::from("Hello");
+        let b = StringPtr::from(" ");
+        let c = StringPtr::from("world!");
+
+        let combined = string_append(a, b);
+        let combined = string_append(combined, c);
+
+        assert_eq!(&*combined, "Hello world!");
     }
 }
