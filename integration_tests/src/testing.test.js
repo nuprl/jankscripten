171b595fb771b041932b7dd013c5f4417fcdc87a
Arjun Guha <arjun@guha-anderson.com>
2021-07-01 19:40:42 -0400

    Support for Windows

--- integration_tests/src/testing.test.js
+++ integration_tests/src/testing.test.js
@@ -1,7 +1,13 @@
 const fs = require('fs');
 const cp = require('child_process');
+const path = require('path');
 
 const testDir = 'test_data';
+
+// Normalize string.
+function normalizeStr(text) {
+  return text.replace(/\r\n/g, '\n').trim();
+}
 
 function makeTest(filename) {
     test(filename, () => {
@@ -12,24 +18,27 @@ function makeTest(filename) {
     
         // Use -j for jankyscript output
         // Use -n for notwasm output
-        cp.execSync(`../bin/jankscripten compile -o ${wasmPath} ${jsPath}`,
-            { stdio: 'inherit' });
 
+        let root = path.dirname(path.dirname(__dirname));
+        let jankscriptenPath = path.join(root, 'target', 'debug', 'jankscripten');
+
+        cp.spawnSync(jankscriptenPath, ['compile', '-o', path.join(wasmPath), path.join(jsPath)], { stdio: 'inherit' });
         let output;
         try {
-            output = String(cp.execSync(`../bin/run-node ${wasmPath}`)).trim();
+            output = String(cp.execSync(`node ../bin/run-node ${wasmPath}`, { stderr: 'inherit' })).trim();
         } catch (e) {
             // jest is very stubborn about printing in the right spot only
             // if it's a thrown error, and the captured stdout won't be seen
             // if we don't stick it in here
             throw new Error(
-`stdout:
-${e.stdout}
-stderr:
-${e.stderr}`);
+                `stdout:
+                ${e.stdout}
+                stderr:
+                ${e.stderr}`);
         }
     
-        let expectedOutput = String(fs.readFileSync(expectedOutputPath)).trim();
+        output = normalizeStr(output);
+        let expectedOutput = normalizeStr(String(fs.readFileSync(expectedOutputPath)));
         expect(output).toBe(expectedOutput);
         fs.unlinkSync(wasmPath);
     });
