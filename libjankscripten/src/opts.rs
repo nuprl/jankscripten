75b19b1f9b363580ee62393d2111da94317228ee
cosine <trash@cosine.online>
2022-01-10 07:32:50 -0500

    Hide typeinf behind flag; allow flags in tests
    The plan is to add the flag to integration tests one by one as we add features
    to typeinf
    
    Just add a line starting with //! and anything on that line will be added as
    arguments to the compiler

--- libjankscripten/src/opts.rs
+++ libjankscripten/src/opts.rs
@@ -4,6 +4,9 @@ pub struct Opts {
     pub disable_gc: bool,
     /// The source code of the NotWasm standard library
     pub notwasm_stdlib_source_code: String,
+    /// Type inference should eventually always be enabled, but is not yet
+    /// fully supported
+    pub typeinf: bool,
 }
 
 impl Opts {
@@ -11,6 +14,7 @@ impl Opts {
         Opts {
             disable_gc: false,
             notwasm_stdlib_source_code: "".to_string(),
+            typeinf: false,
         }
     }
 }
