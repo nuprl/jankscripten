40e5ceaaa3be851c40bbb62c6afed5e0ae93b9c9
cosine <trash@cosine.online>
2022-03-28 20:38:16 -0400

    Remove extraneous op argument types

--- libjankscripten/src/jankyscript/walk.rs
+++ libjankscripten/src/jankyscript/walk.rs
@@ -258,13 +258,10 @@ where
                 self.walk_stmt(&mut *f.body, &loc);
                 self.visitor.exit_fn(f, &loc);
             }
-            JsOp(_, es, JsOpTypeinf { arg_ts, .. }, _) => {
+            JsOp(_, es, _, _) => {
                 let loc = Loc::Node(Context::Expr, loc);
                 for e in es {
                     self.walk_expr(e, &loc);
-                }
-                for t in arg_ts {
-                    self.walk_type(t, &loc);
                 }
             }
             // 1x[Expr]
